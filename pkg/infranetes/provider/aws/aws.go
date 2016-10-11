7d32eea3af25957efd617a5ee8e6de8e8d8ce300
Shaya Potter <spotter@gmail.com>
2016-10-10 23:10:40 -0700

    Big set of changes/refactor to pull out common code belong to all pod providers
    - also improves locking

--- pkg/infranetes/provider/aws/aws.go
+++ pkg/infranetes/provider/aws/aws.go
@@ -6,40 +6,24 @@ import (
 	"io/ioutil"
 	"path/filepath"
 	"strings"
-	"sync"
 	"time"
-
-	"github.com/golang/glog"
-
-	"github.com/apcera/libretto/ssh"
-	lvm "github.com/apcera/libretto/virtualmachine"
-	"github.com/apcera/libretto/virtualmachine/aws"
 
 	"github.com/sjpotter/infranetes/pkg/infranetes/provider"
 	"github.com/sjpotter/infranetes/pkg/infranetes/provider/common"
+
+	"github.com/apcera/libretto/ssh"
+	"github.com/apcera/libretto/virtualmachine/aws"
+	"github.com/golang/glog"
 
 	kubeapi "k8s.io/kubernetes/pkg/kubelet/api/v1alpha1/runtime"
 )
 
 type podData struct {
-	vm                 *aws.VM
-	metadata           *kubeapi.PodSandboxMetadata
-	annotations        map[string]string
-	createdAt          int64
-	ip                 string
-	labels             map[string]string
-	linux              *kubeapi.LinuxPodSandboxConfig
-	stateLock          sync.Mutex
-	client             *common.Client
-	podState           kubeapi.PodSandBoxState
-	vmState            string
 	vmStateLastChecked time.Time
 }
 
 type awsProvider struct {
-	vmMap     map[string]*podData
-	vmMapLock sync.Mutex
-	config    *awsConfig
+	config *awsConfig
 }
 
 func init() {
@@ -75,23 +59,26 @@ func NewAWSProvider() (provider.PodProvider, error) {
 	glog.Infof("Validated AWS Credentials")
 
 	return &awsProvider{
-		vmMap:  make(map[string]*podData),
 		config: &conf,
 	}, nil
 }
 
-func (v *awsProvider) getPodData(id string) (*podData, error) {
-	v.vmMapLock.Lock()
-	defer v.vmMapLock.Unlock()
-
-	podData, ok := v.vmMap[id]
+func (*awsProvider) UpdatePodState(cPodData *common.PodData) {
+	podData, ok := cPodData.ProviderData.(*podData)
 	if !ok {
-		return nil, fmt.Errorf("Invalid PodSandboxId (%v)", id)
+		glog.Warningf("UpdateVMState: Couldn't get ProviderData")
+		return
 	}
-	return podData, nil
+
+	if time.Now().After(podData.vmStateLastChecked.Add(30 * time.Second)) {
+		err := cPodData.UpdatePodState()
+		if err == nil {
+			podData.vmStateLastChecked = time.Now()
+		}
+	}
 }
 
-func (v *awsProvider) RunPodSandbox(req *kubeapi.RunPodSandboxRequest) (*kubeapi.RunPodSandboxResponse, error) {
+func (v *awsProvider) RunPodSandbox(req *kubeapi.RunPodSandboxRequest) (*common.PodData, error) {
 	rawKey, err := ioutil.ReadFile(v.config.SshKey)
 	if err != nil {
 		return nil, fmt.Errorf("failed to read key: %v\n", err)
@@ -136,242 +123,21 @@ func (v *awsProvider) RunPodSandbox(req *kubeapi.RunPodSandboxRequest) (*kubeapi
 	if err != nil {
 		return nil, fmt.Errorf("CreatePodSandbox: error in createClient(): %v", err)
 	}
-	if client == nil {
-		glog.Infof("WARNING WARNING WARNING: returned a nil GRPC client")
-		glog.Infof("WARNING WARNING WARNING: returned a nil GRPC client")
-		glog.Infof("WARNING WARNING WARNING: returned a nil GRPC client")
-		vm.Destroy()
-		return nil, fmt.Errorf("returned a nil GRPC client")
-	}
 
-	v.vmMapLock.Lock()
-
-	v.vmMap[name] = &podData{
-		vm:                 vm,
-		metadata:           req.Config.Metadata,
-		annotations:        req.Config.Annotations,
-		createdAt:          time.Now().Unix(),
-		ip:                 ip,
-		labels:             req.Config.Labels,
-		linux:              req.Config.Linux,
-		client:             client,
-		podState:           kubeapi.PodSandBoxState_READY,
-		vmState:            lvm.VMRunning,
+	providerData := &podData{
 		vmStateLastChecked: time.Now(),
 	}
 
-	v.vmMapLock.Unlock()
+	podData := common.NewPodData(vm, &name, req.Config.Metadata, req.Config.Annotations, req.Config.Labels, ip, client, providerData)
 
-	resp := &kubeapi.RunPodSandboxResponse{
-		PodSandboxId: &name,
-	}
-
-	return resp, nil
+	return podData, nil
 }
 
-func (v *awsProvider) StopPodSandbox(req *kubeapi.StopPodSandboxRequest) (*kubeapi.StopPodSandboxResponse, error) {
-	podData, err := v.getPodData(req.GetPodSandboxId())
-	if err != nil {
-		return nil, fmt.Errorf("StopPodSandbox: %v", err)
-	}
-
-	podData.stateLock.Lock()
-	podData.podState = kubeapi.PodSandBoxState_NOTREADY
-	podData.stateLock.Unlock()
-
-	resp := &kubeapi.StopPodSandboxResponse{}
-	return resp, nil
+func (v *awsProvider) StopPodSandbox(podData *common.PodData) {
 }
 
-func (v *awsProvider) RemovePodSandbox(req *kubeapi.RemovePodSandboxRequest) (*kubeapi.RemovePodSandboxResponse, error) {
-	podData, err := v.getPodData(req.GetPodSandboxId())
-	if err != nil {
-		return nil, fmt.Errorf("RemovePodSandbox: %v", err)
-	}
-
-	if err := podData.vm.Destroy(); err != nil {
-		return nil, fmt.Errorf("RemovePodSandbox: %v", err)
-	}
-
-	podData.client.Close()
-
-	v.vmMapLock.Lock()
-	delete(v.vmMap, req.GetPodSandboxId())
-	v.vmMapLock.Unlock()
-
-	resp := &kubeapi.RemovePodSandboxResponse{}
-	return resp, nil
+func (v *awsProvider) RemovePodSandbox(podData *common.PodData) {
 }
 
-func updateVMState(podData *podData) string {
-	podData.stateLock.Lock()
-
-	ret := podData.vmState
-
-	if time.Now().After(podData.vmStateLastChecked.Add(30 * time.Second)) {
-		vmState, err := podData.vm.GetState()
-		if err == nil {
-			podData.vmState = vmState
-			podData.vmStateLastChecked = time.Now()
-			ret = podData.vmState
-		}
-	}
-
-	podData.stateLock.Unlock()
-
-	return ret
-}
-
-func (v *awsProvider) PodSandboxStatus(req *kubeapi.PodSandboxStatusRequest) (*kubeapi.PodSandboxStatusResponse, error) {
-	podData, err := v.getPodData(req.GetPodSandboxId())
-	if err != nil {
-		return nil, fmt.Errorf("PodSandboxStatus: %v", err)
-	}
-
-	network := &kubeapi.PodSandboxNetworkStatus{
-		Ip: &podData.ip,
-	}
-
-	vmState := updateVMState(podData)
-
-	state := podData.podState
-	if vmState != lvm.VMRunning {
-		state = kubeapi.PodSandBoxState_NOTREADY
-	}
-
-	id := req.GetPodSandboxId()
-
-	net := "host"
-	linux := &kubeapi.LinuxPodSandboxStatus{
-		Namespaces: &kubeapi.Namespace{
-			Network: &net,
-			Options: podData.linux.NamespaceOptions,
-		},
-	}
-
-	status := &kubeapi.PodSandboxStatus{
-		Id:          &id,
-		CreatedAt:   &podData.createdAt,
-		Metadata:    podData.metadata,
-		Network:     network,
-		State:       &state,
-		Linux:       linux,
-		Labels:      podData.labels,
-		Annotations: podData.annotations,
-	}
-
-	resp := &kubeapi.PodSandboxStatusResponse{
-		Status: status,
-	}
-
-	return resp, nil
-}
-
-func (v *awsProvider) ListPodSandbox(req *kubeapi.ListPodSandboxRequest) (*kubeapi.ListPodSandboxResponse, error) {
-	sandboxes := []*kubeapi.PodSandbox{}
-
-	v.vmMapLock.Lock()
-	defer v.vmMapLock.Unlock()
-
-	glog.V(1).Infof("ListPodSandbox: len of vmMap = %v", len(v.vmMap))
-
-	for id, podData := range v.vmMap {
-		glog.V(1).Infof("ListPodSandbox:v podData for %v = %+v", id, podData)
-		vmState := updateVMState(podData)
-
-		if vmState == "terminated" || vmState == "shutting-down" {
-			glog.V(1).Infof("ListPodSandbox: filtering out %v because the vm is no longer available", id)
-			continue
-		}
-
-		podState := podData.podState
-		if vmState != lvm.VMRunning {
-			podState = kubeapi.PodSandBoxState_NOTREADY
-		}
-
-		if req.Filter != nil {
-			if req.Filter.GetId() != "" && req.Filter.GetId() != id {
-				glog.V(1).Infof("ListPodSandbox: filtering out %v because doesn't match %v", id, req.Filter.GetId())
-				continue
-			}
-
-			if req.Filter.GetState() != podState {
-				glog.V(1).Infof("ListPodSandbox: filtering out %v because want %v and got %v", id, req.Filter.GetState(), podState)
-				continue
-			}
-
-			filterLabels := req.Filter.GetLabelSelector()
-
-			if filter, msg := filterByLabels(filterLabels, podData.labels); filter {
-				glog.V(1).Infof("ListPodSandbox: filtering out %v on labels as %v", id, msg)
-				continue
-			}
-		}
-
-		sandbox := &kubeapi.PodSandbox{
-			CreatedAt:   &podData.createdAt,
-			Id:          &id,
-			Metadata:    podData.metadata,
-			Labels:      podData.labels,
-			Annotations: podData.annotations,
-			State:       &podState,
-		}
-
-		glog.V(1).Infof("ListPodSandbox Appending a sandbox for %v to sandboxes", id)
-
-		sandboxes = append(sandboxes, sandbox)
-	}
-
-	glog.V(1).Infof("ListPodSandbox: len of sandboxes returning = %v", len(sandboxes))
-
-	resp := &kubeapi.ListPodSandboxResponse{
-		Items: sandboxes,
-	}
-
-	return resp, nil
-}
-
-func filterByLabels(filterLabels map[string]string, podLabels map[string]string) (bool, string) {
-	for key, filterVal := range filterLabels {
-		if podVal, ok := podLabels[key]; !ok {
-			return true, fmt.Sprintf("didn't find key %v in local labels: %+v", key, podLabels)
-		} else {
-			if podVal != filterVal {
-				return true, fmt.Sprintf("key value's didn't match %v and %v", filterVal, podVal)
-			}
-		}
-	}
-
-	return false, ""
-}
-
-func (v *awsProvider) GetIP(podName string) (string, error) {
-	podData, err := v.getPodData(podName)
-
-	if err != nil {
-		return "", fmt.Errorf("%v unknown pod name", podName)
-	}
-
-	return podData.ip, nil
-}
-
-func (v *awsProvider) GetClient(podName string) (*common.Client, error) {
-	podData, err := v.getPodData(podName)
-
-	if err != nil {
-		return nil, fmt.Errorf("%v unknown pod name", podName)
-	}
-
-	return podData.client, nil
-}
-
-func (v *awsProvider) GetVMList() []string {
-	ret := []string{}
-	v.vmMapLock.Lock()
-	for name := range v.vmMap {
-		ret = append(ret, name)
-	}
-	v.vmMapLock.Unlock()
-
-	return ret
+func (v *awsProvider) PodSandboxStatus(podData *common.PodData) {
 }
---
+++ pkg/infranetes/provider/common/poddata.go
@@ -0,0 +1,128 @@
+package common
+
+import (
+	"fmt"
+	"sync"
+	"time"
+
+	lvm "github.com/apcera/libretto/virtualmachine"
+	kubeapi "k8s.io/kubernetes/pkg/kubelet/api/v1alpha1/runtime"
+)
+
+type PodData struct {
+	VM           lvm.VirtualMachine
+	Id           *string
+	Metadata     *kubeapi.PodSandboxMetadata
+	Annotations  map[string]string
+	Labels       map[string]string
+	CreatedAt    int64
+	Ip           string
+	Linux        *kubeapi.LinuxPodSandboxConfig
+	StateLock    sync.Mutex
+	Client       *Client
+	PodState     kubeapi.PodSandBoxState
+	ProviderData interface{}
+}
+
+func NewPodData(vm lvm.VirtualMachine, id *string, meta *kubeapi.PodSandboxMetadata, anno map[string]string,
+	labels map[string]string, ip string, client *Client, providerData interface{}) *PodData {
+	return &PodData{
+		VM:           vm,
+		Id:           id,
+		Metadata:     meta,
+		Annotations:  anno,
+		Labels:       labels,
+		CreatedAt:    time.Now().Unix(),
+		Ip:           ip,
+		Client:       client,
+		PodState:     kubeapi.PodSandBoxState_READY,
+		ProviderData: providerData,
+	}
+
+}
+
+/* Expect StateLock to already be taken */
+func (p *PodData) StopPod() error {
+	p.PodState = kubeapi.PodSandBoxState_NOTREADY
+
+	return nil
+}
+
+func (p *PodData) RemovePod() error {
+	p.Client.Close()
+
+	return nil
+}
+
+func (p *PodData) PodStatus() *kubeapi.PodSandboxStatus {
+	network := &kubeapi.PodSandboxNetworkStatus{
+		Ip: &p.Ip,
+	}
+
+	net := "host"
+	linux := &kubeapi.LinuxPodSandboxStatus{
+		Namespaces: &kubeapi.Namespace{
+			Network: &net,
+			Options: p.Linux.NamespaceOptions,
+		},
+	}
+
+	status := &kubeapi.PodSandboxStatus{
+		Id:          p.Id,
+		CreatedAt:   &p.CreatedAt,
+		Metadata:    p.Metadata,
+		Network:     network,
+		Linux:       linux,
+		Labels:      p.Labels,
+		Annotations: p.Annotations,
+		State:       &p.PodState,
+	}
+
+	return status
+}
+
+func (p *PodData) Filter(filter *kubeapi.PodSandboxFilter) (bool, string) {
+	if filter != nil {
+		if filter.GetId() != "" && filter.GetId() != *p.Id {
+			return true, fmt.Sprintf("doesn't match %v", filter.GetId())
+		}
+
+		if filter.GetState() != p.PodState {
+			return true, fmt.Sprintf("want %v and got %v", filter.GetState(), p.PodState)
+		}
+
+		for key, filterVal := range filter.GetLabelSelector() {
+			if podVal, ok := p.Labels[key]; !ok {
+				return true, fmt.Sprintf("didn't find key %v in local labels: %+v", key, p.Labels)
+			} else {
+				if podVal != filterVal {
+					return true, fmt.Sprintf("key value's didn't match %v and %v", filterVal, podVal)
+				}
+			}
+		}
+	}
+
+	return false, ""
+}
+
+func (p *PodData) GetSandbox() *kubeapi.PodSandbox {
+	return &kubeapi.PodSandbox{
+		CreatedAt:   &p.CreatedAt,
+		Id:          p.Id,
+		Metadata:    p.Metadata,
+		Labels:      p.Labels,
+		Annotations: p.Annotations,
+		State:       &p.PodState,
+	}
+}
+
+func (p *PodData) UpdatePodState() error {
+	vmState, err := p.VM.GetState()
+	if err == nil {
+		if vmState != lvm.VMRunning {
+			p.PodState = kubeapi.PodSandBoxState_NOTREADY
+		}
+	}
+
+	return err
+}
