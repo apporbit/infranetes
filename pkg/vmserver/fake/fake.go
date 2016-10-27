769fec28f2ca00af12c10c18381dd6e3cf44550e
Shaya Potter <spotter@gmail.com>
2016-10-27 10:17:43 -0700

    add image selection to aws and tweak fake support

--- pkg/vmserver/fake/fake.go
+++ pkg/vmserver/fake/fake.go
@@ -6,6 +6,8 @@ import (
 	"sync"
 	"time"
 
+	"github.com/golang/glog"
+
 	"github.com/sjpotter/infranetes/pkg/vmserver"
 
 	kubeapi "k8s.io/kubernetes/pkg/kubelet/api/v1alpha1/runtime"
@@ -26,17 +28,19 @@ type container struct {
 }
 
 func (c *container) toKubeContainer() *kubeapi.Container {
-	return &kubeapi.Container{
-		PodSandboxId: c.podId,
-		Metadata:     c.metadata,
+	ret := &kubeapi.Container{
 		Annotations:  c.annotations,
 		CreatedAt:    &c.createdAt,
 		Id:           c.id,
 		Image:        c.image,
 		ImageRef:     c.image.Image,
 		Labels:       c.labels,
+		Metadata:     c.metadata,
+		PodSandboxId: c.podId,
 		State:        &c.state,
 	}
+
+	return ret
 }
 
 func (c *container) toKubeStatus() *kubeapi.ContainerStatus {
@@ -45,21 +49,24 @@ func (c *container) toKubeStatus() *kubeapi.ContainerStatus {
 	if c.state == kubeapi.ContainerState_EXITED {
 		reason = "Completed"
 	}
-	return &kubeapi.ContainerStatus{
+
+	ret := &kubeapi.ContainerStatus{
+		Annotations: c.annotations,
+		CreatedAt:   &c.createdAt,
+		ExitCode:    &exitCode,
+		FinishedAt:  &c.FinishedAt,
 		Id:          c.id,
-		Metadata:    c.metadata,
 		Image:       c.image,
 		ImageRef:    c.image.Image,
-		Mounts:      c.mounts,
-		ExitCode:    &exitCode,
-		State:       &c.state,
-		CreatedAt:   &c.createdAt,
-		StartedAt:   &c.startedAt,
-		FinishedAt:  &c.FinishedAt,
-		Reason:      &reason,
 		Labels:      c.labels,
-		Annotations: c.annotations,
+		Metadata:    c.metadata,
+		Mounts:      c.mounts,
+		Reason:      &reason,
+		StartedAt:   &c.startedAt,
+		State:       &c.state,
 	}
+
+	return ret
 }
 
 type fakeProvider struct {
@@ -79,12 +86,12 @@ func NewFakeProvider() (vmserver.ContainerProvider, error) {
 	return fakeProvider, nil
 }
 
-func (d *fakeProvider) CreateContainer(req *kubeapi.CreateContainerRequest) (*kubeapi.CreateContainerResponse, error) {
-	d.mapLock.Lock()
-	defer d.mapLock.Unlock()
+func (f *fakeProvider) CreateContainer(req *kubeapi.CreateContainerRequest) (*kubeapi.CreateContainerResponse, error) {
+	f.mapLock.Lock()
+	defer f.mapLock.Unlock()
 
 	id := req.GetPodSandboxId() + ":" + req.Config.Metadata.GetName()
-	d.contMap[id] = &container{
+	f.contMap[id] = &container{
 		id:          &id,
 		podId:       req.PodSandboxId,
 		state:       kubeapi.ContainerState_CREATED,
@@ -99,12 +106,12 @@ func (d *fakeProvider) CreateContainer(req *kubeapi.CreateContainerRequest) (*ku
 	return &kubeapi.CreateContainerResponse{ContainerId: &id}, nil
 }
 
-func (d *fakeProvider) StartContainer(req *kubeapi.StartContainerRequest) (*kubeapi.StartContainerResponse, error) {
-	d.mapLock.Lock()
-	defer d.mapLock.Unlock()
+func (f *fakeProvider) StartContainer(req *kubeapi.StartContainerRequest) (*kubeapi.StartContainerResponse, error) {
+	f.mapLock.Lock()
+	defer f.mapLock.Unlock()
 
 	id := req.GetContainerId()
-	if cont, ok := d.contMap[id]; !ok {
+	if cont, ok := f.contMap[id]; !ok {
 		return nil, fmt.Errorf("StartContainer: Invalid ContainerID: %v", id)
 	} else {
 		cont.state = kubeapi.ContainerState_RUNNING
@@ -113,41 +120,40 @@ func (d *fakeProvider) StartContainer(req *kubeapi.StartContainerRequest) (*kube
 	}
 }
 
-func (d *fakeProvider) StopContainer(req *kubeapi.StopContainerRequest) (*kubeapi.StopContainerResponse, error) {
-	d.mapLock.Lock()
-	defer d.mapLock.Unlock()
+func (f *fakeProvider) StopContainer(req *kubeapi.StopContainerRequest) (*kubeapi.StopContainerResponse, error) {
+	f.mapLock.Lock()
+	defer f.mapLock.Unlock()
 
 	id := req.GetContainerId()
-	if cont, ok := d.contMap[id]; !ok {
+	if cont, ok := f.contMap[id]; !ok {
 		return nil, fmt.Errorf("StopContainer: Invalid ContainerID: %v", id)
 	} else {
 		cont.state = kubeapi.ContainerState_EXITED
 		cont.FinishedAt = time.Now().Unix()
 		return &kubeapi.StopContainerResponse{}, nil
 	}
-
 }
 
-func (d *fakeProvider) RemoveContainer(req *kubeapi.RemoveContainerRequest) (*kubeapi.RemoveContainerResponse, error) {
-	d.mapLock.Lock()
-	defer d.mapLock.Unlock()
+func (f *fakeProvider) RemoveContainer(req *kubeapi.RemoveContainerRequest) (*kubeapi.RemoveContainerResponse, error) {
+	f.mapLock.Lock()
+	defer f.mapLock.Unlock()
 
 	id := req.GetContainerId()
-	if _, ok := d.contMap[id]; !ok {
+	if _, ok := f.contMap[id]; !ok {
 		return nil, fmt.Errorf("RemoveContainer: Invalid ContainerID: %v", id)
 	} else {
-		delete(d.contMap, id)
+		delete(f.contMap, id)
 		return &kubeapi.RemoveContainerResponse{}, nil
 	}
 }
 
-func (d *fakeProvider) ListContainers(req *kubeapi.ListContainersRequest) (*kubeapi.ListContainersResponse, error) {
-	d.mapLock.Lock()
-	defer d.mapLock.Unlock()
+func (f *fakeProvider) ListContainers(req *kubeapi.ListContainersRequest) (*kubeapi.ListContainersResponse, error) {
+	f.mapLock.Lock()
+	defer f.mapLock.Unlock()
 
 	containers := []*kubeapi.Container{}
 
-	for _, cont := range d.contMap {
+	for _, cont := range f.contMap {
 		if filter(req.Filter, cont) {
 			continue
 		}
@@ -164,15 +170,23 @@ func (d *fakeProvider) ListContainers(req *kubeapi.ListContainersRequest) (*kube
 func filter(filter *kubeapi.ContainerFilter, cont *container) bool {
 	if filter != nil {
 		if filter.GetId() != "" && filter.GetId() == *cont.id {
+			glog.Infof("Filtering out %v as want %v", *cont.id, filter.GetId())
 			return true
 		}
 
 		if filter.GetState() == cont.state {
+			glog.Infof("Filtering out %v as want %v and got %v", *cont.id, filter.GetState(), cont.state)
+			return true
+		}
+
+		if filter.GetPodSandboxId() != "" && filter.GetPodSandboxId() != *cont.podId {
+			glog.Infof("Filtering out %v as want %v and got %v", *cont.id, filter.GetPodSandboxId(), *cont.podId)
 			return true
 		}
 
 		for k, v := range filter.GetLabelSelector() {
 			if cont.labels[k] != v {
+				glog.Infof("Filtering out %v as want labels[%v] = %v and got %v", *cont.id, k, v, cont.labels[k])
 				return true
 			}
 		}
@@ -181,12 +195,12 @@ func filter(filter *kubeapi.ContainerFilter, cont *container) bool {
 	return false
 }
 
-func (d *fakeProvider) ContainerStatus(req *kubeapi.ContainerStatusRequest) (*kubeapi.ContainerStatusResponse, error) {
-	d.mapLock.Lock()
-	defer d.mapLock.Unlock()
+func (f *fakeProvider) ContainerStatus(req *kubeapi.ContainerStatusRequest) (*kubeapi.ContainerStatusResponse, error) {
+	f.mapLock.Lock()
+	defer f.mapLock.Unlock()
 
 	id := req.GetContainerId()
-	if cont, ok := d.contMap[id]; !ok {
+	if cont, ok := f.contMap[id]; !ok {
 		return nil, fmt.Errorf("ContainerStatus: Invalid ContainerID: %v", id)
 	} else {
 		resp := &kubeapi.ContainerStatusResponse{
@@ -197,6 +211,6 @@ func (d *fakeProvider) ContainerStatus(req *kubeapi.ContainerStatusRequest) (*ku
 	}
 }
 
-func (d *fakeProvider) Exec(_ kubeapi.RuntimeService_ExecServer) error {
+func (f *fakeProvider) Exec(_ kubeapi.RuntimeService_ExecServer) error {
 	return errors.New("unimplemented")
 }
